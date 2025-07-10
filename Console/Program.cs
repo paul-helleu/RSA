c59dcf9eb5f431d8fe86c6305e6737f0496697e4
paul-helleu <paul.helleu@orange.fr>
2025-07-11 01:19:56 +0200

    feat: generate p and q and stopwatch generation

---
+++ Console/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Security.Cryptography;
+using Core;
+
+var myRng = new MyRandomNumberGenerator(RandomNumberGenerator.Create());
+
+var stopwatch = Stopwatch.StartNew();
+var p = myRng.GenerateProbablyPrimeNumber(8, 10);
+stopwatch.Stop();
+
+Console.WriteLine($"p: {p} is probably prime, generated in {stopwatch.ElapsedTicks} ticks!");
+
+stopwatch.Restart();
+var q = myRng.GenerateProbablyPrimeNumber(8, 10);
+stopwatch.Stop();
+
+Console.WriteLine($"q: {q} is probably prime, generated in {stopwatch.ElapsedTicks} ticks!");
